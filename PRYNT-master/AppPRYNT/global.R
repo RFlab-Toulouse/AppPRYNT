e3c7278e50d478c12e8eadfe48d0474ed3fc708b
RFlab-Toulouse <joost-peter.schanstra@inserm.fr>
2025-01-08 13:43:31 +0100

    commit

--- PRYNT-master/AppPRYNT/global.R
+++ PRYNT-master/AppPRYNT/global.R
@@ -24,10 +24,7 @@ usePackage_bioconductor <- function(p)
 if (!requireNamespace("devtools", quietly = TRUE)) {
   install.packages("devtools")
 }
-#devtools::install_github("PRYNT-master/AppPRYNT/RandomWalkRestartMH/RandomWalkRestartMH_new")
-system("git clone https://git.bioconductor.org/packages/RandomWalkRestartMH")
-devtools::install("RandomWalkRestartMH")
-
+devtools::install_github("PRYNT-master/AppPRYNT/RandomWalkRestartMH/RandomWalkRestartMH_new")
 library(RandomWalkRestartMH)
 
 # usePackage("RandomWalkRestartMH")
